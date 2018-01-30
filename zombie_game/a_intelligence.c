6678525f806ad8ada63ed3b163bd2311a07eb0ba
pedrooliveira2007 <33415851+pedrooliveira2007@users.noreply.github.com>
2018-01-30 22:22:41 +0000

    Update a_intelligence.c

--- zombie_game/a_intelligence.c
+++ zombie_game/a_intelligence.c
@@ -6,8 +6,33 @@
 
 
 
-BOARD a_int (AGENT **agent_grid, unsigned int key){
+BOARD a_int ( unsigned int key){
 
+  key= ((rand() % 9)+1 );
+  switch(key)
+  {
+    case 1:{key= ldown ; break;}
+      
+    case 2:{key= down ; break;}
+      
+    case 3:{key= rdown ; break;}
+      
+    case 4:{key= left ; break;}
+      
+    case 5:{key= pass ; break;}
+      
+    case 6:{key= right ; break;}
+      
+    case 7:{key= lup ; break;}
+      
+    case 8:{key= up ; break;}
+      
+    case 9:{key= rup ; break;}
+      
+    default: break;  
+      
+  }
+ 
 
 return key;
 
